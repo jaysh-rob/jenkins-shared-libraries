019a12b368502b7b1070f2a9df62c5349c4e3495
jaysh-rob <jasonroys210@gmail.com>
2025-10-18 08:49:54 +0100

    lower case variable for docker

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
-def call(string user_name, string project_name, string tag_name){
+def call(String user_name, String project_name, String tag_name) {
     sh """
         docker build -t ${user_name}/${project_name}:${tag_name} .
     """
